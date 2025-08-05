eff30f61db29fdfbccc7e1705b151a290c0601d7
Matteo Leccese <matteoleccese2099@gmail.com>
2025-08-05 13:59:22 -0400

    adding a fallback to show a custom error if a route dont match

--- src/app.ts
+++ src/app.ts
@@ -10,4 +10,9 @@ app.use(express.json());
 // Adding the 'workers' routes to the app using 'api' as base
 app.use("/api/workers", workerRoutes);
 
+// Fallback for unmatched routes
+app.use((_req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
 export default app;
